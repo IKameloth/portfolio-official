b858d19c6510b5a08a2627b4707a0f3e6670e883
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2024-04-08 13:26:39 -0400

    :hammer: update cv and remove images unused

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -51,6 +51,38 @@ export default function Experience() {
             {t("last_job.description")}
           </p>
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          visible={true}
+          contentStyle={{
+            background:
+              theme === "light" ? "#f3f4f6" : "rgb(255 255 255 / 0.1)",
+            boxShadow: "none",
+            border: "1px solid rgb(0 0 0 / 0.05)",
+            textAlign: "left",
+            padding: "1.3rem 2rem",
+          }}
+          contentArrowStyle={{
+            borderRight:
+              theme === "light"
+                ? "0.4rem solid #9ca3af"
+                : "0.4rem solid rgba(255, 255, 255, 0.5)",
+          }}
+          date={t("second_job.date")}
+          icon={<CgWorkAlt />}
+          iconStyle={{
+            background: theme === "light" ? "#f3f4f6" : "#1f2937",
+            fontSize: "1.5rem",
+            color: theme === "light" ? "#1f2937" : "#f3f4f6",
+          }}
+        >
+          <h3 className="font-semibold capitalize">{t("second_job.title")}</h3>
+          <p className="font-normal !mt-0">{t("second_job.location")}</p>
+          <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+            {t("second_job.description")}
+          </p>
+        </VerticalTimelineElement>
+
         <VerticalTimelineElement
           visible={true}
           contentStyle={{
@@ -81,6 +113,7 @@ export default function Experience() {
             {t("third_job.description")}
           </p>
         </VerticalTimelineElement>
+
         <VerticalTimelineElement
           visible={true}
           contentStyle={{
@@ -109,36 +142,6 @@ export default function Experience() {
           <p className="font-normal !mt-0">{t("fourth_job.location")}</p>
           <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
             {t("fourth_job.description")}
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          visible={true}
-          contentStyle={{
-            background:
-              theme === "light" ? "#f3f4f6" : "rgb(255 255 255 / 0.1)",
-            boxShadow: "none",
-            border: "1px solid rgb(0 0 0 / 0.05)",
-            textAlign: "left",
-            padding: "1.3rem 2rem",
-          }}
-          contentArrowStyle={{
-            borderRight:
-              theme === "light"
-                ? "0.4rem solid #9ca3af"
-                : "0.4rem solid rgba(255, 255, 255, 0.5)",
-          }}
-          date={t("second_job.date")}
-          icon={<CgWorkAlt />}
-          iconStyle={{
-            background: theme === "light" ? "#f3f4f6" : "#1f2937",
-            fontSize: "1.5rem",
-            color: theme === "light" ? "#1f2937" : "#f3f4f6",
-          }}
-        >
-          <h3 className="font-semibold capitalize">{t("second_job.title")}</h3>
-          <p className="font-normal !mt-0">{t("second_job.location")}</p>
-          <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-            {t("second_job.description")}
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
